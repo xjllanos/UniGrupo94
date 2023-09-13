926317534c75a44279c13b5dc6061d9a520ceb13
xjllanos <soledadllanos639@gmail.com>
2023-09-13 18:49:21 -0300

    .

---
+++ src/unigrupo94/Entidades/Materia.java
@@ -0,0 +1,66 @@
+
+package unigrupo94.Entidades;
+
+
+public class Materia {
+    
+    private int idMateria; 
+    private String nombre;
+    private int anio; 
+    private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anio, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int anio, boolean activo) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anio=" + anio + '}';
+    }
+    
+    
+}
