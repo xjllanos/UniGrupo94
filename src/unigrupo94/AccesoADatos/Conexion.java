5ab35ed6bb4efe84de500c548fddac013207730a
xjllanos <soledadllanos639@gmail.com>
2023-09-13 19:16:04 -0300

    .

--- src/unigrupo94/AccesoADatos/Conexion.java
+++ src/unigrupo94/AccesoADatos/Conexion.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package unigrupo94.AccesoADatos;
 
-/**
- *
- * @author JATil
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
 public class Conexion {
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB = "uni94";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = ""; 
+    private static Connection connection ; 
     
+    private Conexion () {}
+    
+    public static Connection getConexion (){
+    
+        if (connection == null){
+            
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD); 
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, " Error al cargar los driver ");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, " Error al conectarse a la BD");
+            }
+        }
+        
+        return connection ; 
+    }
 }
--- src/unigrupo94/UniGrupo94.java
+++ src/unigrupo94/UniGrupo94.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package unigrupo94;
 
-/**
- *
- * @author JATil
- */
+import java.sql.Connection;
+import unigrupo94.AccesoADatos.Conexion;
+
+
 public class UniGrupo94 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Connection con = Conexion.getConexion(); 
+        
     }
     
 }
