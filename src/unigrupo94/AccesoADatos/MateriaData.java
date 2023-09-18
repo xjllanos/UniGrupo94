29f813cbe2d0fc43d461a50af58de0180a31cede
guido <104656658+guido-nunez@users.noreply.github.com>
2023-09-18 19:40:34 -0300

    Update MateriaData.java

--- src/unigrupo94/AccesoADatos/MateriaData.java
+++ src/unigrupo94/AccesoADatos/MateriaData.java
@@ -7,6 +7,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
